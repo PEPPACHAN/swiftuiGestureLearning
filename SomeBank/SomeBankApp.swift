c7d090011561a33f99284f6b1cf247a270812150
PEPPACHAN <PEPPACHAN@users.noreply.github.com>
2024-05-22 12:22:46 +0300

    Initial Commit

---
+++ SomeBank/SomeBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeBankApp.swift
+//  SomeBank
+//
+//  Created by PEPPA CHAN on 22.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SomeBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
