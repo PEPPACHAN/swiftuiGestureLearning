06623f3a3b2b0904cd20ddbde036703225aa6be9
PEPPACHAN <PEPPACHAN@users.noreply.github.com>
2024-05-24 09:27:29 +0300

    learning to make a unclosing sheet for next pet

--- SomeBank/ContentView.swift
+++ SomeBank/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var offsetSubPage = CGPoint(x: 0, y: UIScreen.main.bounds.height / 1.5)
+    @State private var offsetText: CGPoint = .zero
+    let screenSize = UIScreen.main.bounds
+    @State private var arr = ["arr", "arr", "arr", "arr", "arr", "arr", "arr", "arr"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            ScrollView(.horizontal){
+                HStack {
+                    ForEach(arr, id: \.self){
+                        Text("\($0)")
+                            .padding(20)
+                            .font(.largeTitle)
+                            .background(Color.green)
+                    }
+                }
+            }
+            SubPage()
+                .cornerRadius(30)
+                .offset(CGSize(width: offsetSubPage.x, height: offsetSubPage.y))
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            offsetSubPage = CGPoint(x: 0, y: value.location.y)
+                        })
+                        .onEnded({ value in
+                            withAnimation {
+                                offsetSubPage = value.translation.height < 20 ? CGPoint(x: 0, y: screenSize.height / 8): CGPoint(x: 0, y: screenSize.height / 1.5)
+                            }}))
+                .onTapGesture {
+                    withAnimation{
+                        offsetSubPage = CGPoint(x: 0, y: screenSize.height / 8)
+                    }}
         }
-        .padding()
+    }
+}
+
+struct SubPage: View {
+    
+    let screenSize = UIScreen.main.bounds
+    
+    var body: some View {
+        VStack{
+            Text("Some New Text")
+                .padding(50)
+            Spacer()
+        }
+        .frame(width: screenSize.width, height: screenSize.height)
+        .background(Color(.red))
+        .font(.largeTitle)
+        .foregroundColor(.white)
     }
 }
 
