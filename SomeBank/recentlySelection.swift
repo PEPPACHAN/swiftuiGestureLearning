dd1df40c2cdd646f9348a9bdc8ad32232d24de87
PEPPACHAN <PEPPACHAN@users.noreply.github.com>
2024-06-06 22:55:59 +0300

    init

---
+++ SomeBank/recentlySelection.swift
@@ -0,0 +1,18 @@
+//
+//  recentlySelection.swift
+//  SomeBank
+//
+//  Created by PEPPA CHAN on 01.06.2024.
+//
+
+import SwiftUI
+
+struct recentlySelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    recentlySelection()
+}
---
+++ SomeBank/subPage.swift
@@ -0,0 +1,18 @@
+//
+//  subPage.swift
+//  SomeBank
+//
+//  Created by PEPPA CHAN on 01.06.2024.
+//
+
+import SwiftUI
+
+struct subPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    subPage()
+}
